fb4ef21890d73588f19773fc67a8c5b2551f4031
KARA PEDERSEN <KJPED74@GMAIL.COM>
2021-08-03 10:55:00 -0700

    added twin peaks container

---
+++ src/containers/TwinPeaksQuotes.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';  
+import Load from '../components/app/quote/Load';
+import Quote from '../components/app/quote/Quote';
+import { fetchQuote } from '../services/twinPeaksApi';
+
+const TwinPeaksQuotes = () => {
+  // eslint-disable-next-line max-len
+  const [quote, setQuote] = useState({ 'quote': 'Get a quote here! Click the button!' });
+
+  const handleClick = async () => {
+    const res = await fetchQuote();
+    const quote = JSON.stringify(res);
+    setQuote({ quote });
+  };
+
+  return (
+    <>
+      <Quote quote={quote} />
+      <Load onClick={handleClick} />
+    </>
+  );
+};
+
+export default TwinPeaksQuotes;
